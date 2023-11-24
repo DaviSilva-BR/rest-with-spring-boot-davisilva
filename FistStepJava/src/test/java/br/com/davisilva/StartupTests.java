89c248ac8969049d88acdaeacdeca41c00c72ce8
Davi <davigabriel.sp@outlook.com>
2023-11-24 11:46:30 -0300

    first commit

---
+++ FistStepJava/src/test/java/br/com/davisilva/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.davisilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
